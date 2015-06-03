2b8e7c88d138e15edfd25985e2f89d81f74a0365
Tan Nhu <tannhu@gmail.com>
2015-06-03 12:16:07 -0700

    Re-implement using CSS

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*jslint vars: true */
-/*global $, document, define, brackets */
+/*global $, define, brackets */
 
 define(function (require, exports, module) {
   'use strict';
@@ -7,56 +7,12 @@ define(function (require, exports, module) {
   var EXTENSION_ID = 'brackets-maximize-editor';
   var CommandManager = brackets.getModule('command/CommandManager');
   var Menus = brackets.getModule('command/Menus');
-  var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
 
-  var left, right;
+  var $mainView = $('.main-view');
+  var className = 'maximize-editor';
 
   function handler() {
-    var $content = $('.content');
-    var firstPanel = $('#first-pane');
-    var secondPanel = $('#second-pane');
-    var active = 'active-pane';
-
-    if ($('#sidebar').is(':hidden')) {
-      $('#sidebar,#main-toolbar').show();
-
-      if (secondPanel.length) {
-        if (firstPanel.hasClass(active)) {
-          firstPanel.width('50%');
-          secondPanel.show();
-        }
-        else {
-          secondPanel.width('auto');
-          firstPanel.show();
-        }
-      }
-      else {
-        firstPanel.width('100%').show();
-      }
-
-      $content.css({ left: left, right: right });
-    }
-    else {
-      $('#sidebar,#main-toolbar').hide();
-
-      if (secondPanel.length) {
-        if (firstPanel.hasClass(active)) {
-          firstPanel.width('100%');
-          secondPanel.hide();
-        }
-        else {
-          secondPanel.width('100%');
-          firstPanel.hide();
-        }
-      }
-      else {
-        firstPanel.width('100%');
-      }
-
-      left = $content.css('left');
-      right = $content.css('right');
-      $content.css({ left: 0, right: 0 });
-    }
+    $mainView.toggleClass(className);
   }
 
   CommandManager.register('Maximize Editor', EXTENSION_ID, handler);
@@ -69,6 +25,9 @@ define(function (require, exports, module) {
     key: "Cmd-Shift-Enter",
     platform: "mac"
   }];
+
+  var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+  ExtensionUtils.loadStyleSheet(module, 'main.css');
 
   menu.addMenuDivider();
   menu.addMenuItem(EXTENSION_ID, command);
